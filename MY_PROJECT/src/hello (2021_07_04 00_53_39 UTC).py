56cb4826c6cf197b2610fd7592f4fa71b675bdfc
YooJaeHyeong <76415274+YooJaeHyeong@users.noreply.github.com>
2021-08-18 11:57:33 +0900

    연습

--- MY_PROJECT/src/hello (2021_07_04 00_53_39 UTC).py
+++ MY_PROJECT/src/hello (2021_07_04 00_53_39 UTC).py
@@ -1,1 +1,59 @@
-print("나의 첫 파이썬 코드 ~!")
+# print("나의 첫 파이썬 코드 ~!")
+#
+# score = int(input("점수를 입력하세요:"))
+#
+# if score >= 60:
+#     print("합격입니다.")
+# else:
+#     print("불합격 입니다.")
+
+
+'''
+import turtle
+t = turtle.Turtle()
+t.shape("turtle")
+
+t.penup()
+t.goto(100, 100)
+t.write("두 수 모두 양수.")
+t.goto(100,-100)
+t.write("두번 째 수만 음수.")
+t.goto(-100, 100)
+t.write("첫 번 째 수만 음수.")
+to.goto(-100, -100)
+t.write("두 수 모두 음수.")
+
+t. goto(0, 0)
+t.pendown()
+
+s = turtle.textinput("", "숫자를 입력하세요: ")
+n = int(s)
+
+ss = turtle.textinput("", "숫자를 입력하세요: ")
+m = int(ss)
+
+if n > 0,  m > 0:
+    t.goto(100, 100)
+
+if n > 0, m < 0 :
+    t.goto(100, -100)
+
+if n < 0, m >0:
+    t.goto(-100, 100)
+
+if n < 0 , m < 0:
+    t.goto(-100, -100)
+
+turtle.done()
+'''
+
+# password = ""
+# while password != "pythonisfun":
+#     password = input("암호를 입력하세요: ")
+# print("**로그인 성공**")
+
+# password = ""
+# if password != "pythonisfun":
+#     password = input("암호를 입력하세요: ")
+# else:
+#     print("**로그인 성공**")
